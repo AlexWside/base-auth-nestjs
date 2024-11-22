93a6639b617e701f88f118f8274c718f33f8e743
Alexsandro Fonseca <60578173+AlexWside@users.noreply.github.com>
2024-11-22 15:06:59 -0400

    Update products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './products.entity';
-import { query } from 'express';
 
 @Injectable()
 export class ProductsService {
@@ -11,24 +10,24 @@ export class ProductsService {
     private readonly productRepository: Repository<Product>,
   ) {}
 
-  // Create
+
   async create(productData: Partial<Product>): Promise<Product> {
     const product = this.productRepository.create(productData);
     return await this.productRepository.save(product);
   }
 
-  // Read - Get all
+
   async findAll(): Promise<Product[]> {
     return await this.productRepository.find();
   }
 
-  // exemplo de consulta SQL
+
   async getExpensiveProducts(): Promise<Product[]> {
     const query = `SELECT * FROM product WHERE price > $1`;
     return await this.productRepository.query(query, [1000]);
   }
 
-  // Read - Get one by ID
+
   async findOne(id: number): Promise<Product> {
     return await this.productRepository.findOneBy({ id });
   }
